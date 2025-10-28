be3bff8a046ca6ff0edec6d6744491da47867a74
Naksh-Rathore <naksh.rathore786@gmail.com>
2025-10-27 21:12:16 -0500

    Reposition includes to separate C++ headers from third party ones

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
+#include <iostream>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
